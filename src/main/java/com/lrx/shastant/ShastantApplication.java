506d4997286f1c71b56425069aba00c5e93eb690
LinRuiiXin <1303058293@qq.com>
2021-03-01 19:56:14 +0800

    Initial commit

---
+++ src/main/java/com/lrx/shastant/ShastantApplication.java
@@ -0,0 +1,13 @@
+package com.lrx.shastant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShastantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShastantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lrx/shastant/ShastantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrx.shastant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShastantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
